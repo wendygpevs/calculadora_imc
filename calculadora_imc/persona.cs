661afd1d3a37e3ed0af3631b7f9564dbbb1790fb
Wendy Valenzuela <wendyguadalupevs@gmail.com>
2024-04-18 15:51:40 -0700

    definir la clase persona

---
+++ calculadora_imc/persona.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculadora_imc
+{
+    internal class persona
+    {
+        public string Nombre { get; set; }
+        public int Edad {  get; set; }
+
+        public float Estatura { get; set; }
+        public float Peso { get; set;}
+
+    }
+}
